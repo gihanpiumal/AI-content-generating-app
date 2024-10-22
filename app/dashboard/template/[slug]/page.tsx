6074155bcd3cf98252b846ad39a0cb879491a170
gihanpiumal <gihanpiumal12345@gmail.com>
2024-10-22 16:14:24 +0530

    search and filter

--- app/dashboard/template/[slug]/page.tsx
+++ app/dashboard/template/[slug]/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from "react";
 import Link from "next/link";
-import { ArrowLeft, Loader2Icon } from "lucide-react";
+import { ArrowLeft, Loader2Icon, Copy } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import template from "@/utils/template";
 import Image from "next/image";
@@ -12,6 +12,7 @@ import { runAi } from "@/actions/ai";
 import "@toast-ui/editor/dist/toastui-editor.css";
 
 import { Editor } from "@toast-ui/react-editor";
+import tost, { toast } from "react-hot-toast";
 
 export interface Template {
   name: string;
@@ -69,54 +70,79 @@ export default function Page({ params }: { params: { slug: string } }) {
     setQuery(e.target.value);
   };
 
+  const handleCopy = async () => {
+    const editorInstance = editorRef.current.getInstance();
+    const c = editorInstance.getMarkdown(); // .getHTML()
+
+    try {
+      await navigator.clipboard.writeText(c);
+      toast.success("Content copied to clipboard");
+    } catch (error) {
+      toast.error("An error occurred. Please try again.");
+    }
+  };
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-5">
-      <div className="col-span-1 bg-slate-100 dark:bg-slate-900 rounded-md border p-5">
-        <div className="flex flex-col gap-3">
-          <Image src={t.icon} alt={t.name} width={50} height={50} />
-          <h2 className="font-medium text-lg">{t.name}</h2>
-          <p className="text-gray-500">{t.desc}</p>
-        </div>
-        <form className="mt-6" onSubmit={handleSubmit}>
-          {t.form.map((item) => (
-            <div className="my-2 flex flex-col gap-2 mb-7">
-              <label className="font-bold pb-5">{item.label}</label>
-              {item.field === "input" ? (
-                <Input
-                  name={item.name}
-                  required={item.required}
-                  onChange={handleChange}
-                />
-              ) : (
-                <Textarea
-                  name={item.name}
-                  required={item.required}
-                  onChange={handleChange}
-                />
-              )}
-            </div>
-          ))}
-          <Button type="submit" className="w-full py-6" disabled={loading}>
-            {loading ? (
-              <Loader2Icon className="animate-spin mr-2" />
-            ) : (
-              "Generate Content"
-            )}
+    <div>
+      <div className="flex justify-between mx-5 mb-3 my-3">
+        <Link href={"/dashboard"}>
+          <Button>
+            <ArrowLeft /> <span className="ml-2">Back</span>
           </Button>
-        </form>
+        </Link>
+
+        <Button onClick={handleCopy}>
+          <Copy /> <span className="ml-2">Copy</span>
+        </Button>
       </div>
-      <div className="col-span-2">
-        <Editor
-          ref={editorRef}
-          initialValue="Generated content will appear here"
-          previewStyle="vertical"
-          height="600px"
-          initialEditType="wysiwyg"
-          useCommandShortcut= {true}
-          // onChange={()=>{
-          //   setContent(editorRef.current.getInstance().getMarkdown())
-          // }}
-        />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-5">
+        <div className="col-span-1 bg-slate-100 dark:bg-slate-900 rounded-md border p-5">
+          <div className="flex flex-col gap-3">
+            <Image src={t.icon} alt={t.name} width={50} height={50} />
+            <h2 className="font-medium text-lg">{t.name}</h2>
+            <p className="text-gray-500">{t.desc}</p>
+          </div>
+          <form className="mt-6" onSubmit={handleSubmit}>
+            {t.form.map((item) => (
+              <div className="my-2 flex flex-col gap-2 mb-7">
+                <label className="font-bold pb-5">{item.label}</label>
+                {item.field === "input" ? (
+                  <Input
+                    name={item.name}
+                    required={item.required}
+                    onChange={handleChange}
+                  />
+                ) : (
+                  <Textarea
+                    name={item.name}
+                    required={item.required}
+                    onChange={handleChange}
+                  />
+                )}
+              </div>
+            ))}
+            <Button type="submit" className="w-full py-6" disabled={loading}>
+              {loading ? (
+                <Loader2Icon className="animate-spin mr-2" />
+              ) : (
+                "Generate Content"
+              )}
+            </Button>
+          </form>
+        </div>
+        <div className="col-span-2">
+          <Editor
+            ref={editorRef}
+            initialValue="Generated content will appear here"
+            previewStyle="vertical"
+            height="600px"
+            initialEditType="wysiwyg"
+            useCommandShortcut={true}
+            // onChange={()=>{
+            //   setContent(editorRef.current.getInstance().getMarkdown())
+            // }}
+          />
+        </div>
       </div>
     </div>
   );
